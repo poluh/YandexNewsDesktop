439b3fcb278e34e8451fef7b81604dd07211f3b6
sergey <s.poluh@ro.ru>
2018-01-24 04:24:46 +0900

    Little changes

--- src/com/application/App.java
+++ src/com/application/App.java
@@ -14,13 +14,16 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.IntStream;
 
 import static com.application.action.ActionEvents.categoriesButton;
@@ -31,13 +34,13 @@ import static com.application.brain.data.GetPages.getListNews;
 public class App extends Application {
 
     private static final String PATH_TO_STYLE = "style/standard.css";
-    public static final String MAIN_PAGE = "https://news.yandex.ru/";
+    private static final String MAIN_PAGE = "https://news.yandex.ru/";
     private static Stage pPrimaryStage = null;
 
     private static GridPane createGrid() {
         GridPane gridPane = new GridPane();
         gridPane.setAlignment(Pos.CENTER);
-        gridPane.setPadding(new Insets(25));
+        gridPane.setPadding(new Insets(15));
         gridPane.setVgap(15);
         gridPane.setHgap(15);
         return gridPane;
@@ -48,13 +51,12 @@ public class App extends Application {
         GridPane leftGrid = new GridPane();
         GridPane mainGrid = new GridPane();
         GridPane supplementalGrid = createGrid();
-        ScrollPane scrollPane = new ScrollPane(supplementalGrid);
 
         leftGrid.getStyleClass().add("left-grid");
         leftGrid.setMinHeight(800);
 
         Label categoryLabel = new Label("Категории");
-        categoryLabel.setId("title");
+        categoryLabel.setId("categories");
 
         HBox topBox = new HBox();
         topBox.setAlignment(Pos.CENTER_LEFT);
@@ -67,13 +69,15 @@ public class App extends Application {
         int i = 0;
         for (Category category : getListCategories(MAIN_PAGE)) {
             Button categoryButton = new Button(category.getName());
-            categoryButton.setMinSize(140, 35);
+            categoryButton.setMinSize(140, 50);
             categoryButton.setId(category.getLink());
             categoriesButton(categoryButton, supplementalGrid);
 
             leftGrid.add(categoryButton, 0, i);
             i++;
         }
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setContent(supplementalGrid);
 
         mainGrid.add(leftGrid, 0, 1);
         mainGrid.add(scrollPane, 1, 1);
@@ -82,18 +86,20 @@ public class App extends Application {
     }
 
     public static void createNewsWindow(News news) {
-
     }
 
-    public static void createCategoriesWindow(String link, GridPane gridPane) throws IOException {
-        for (News news : getListNews(link)) {
 
+    public static void createCategoriesWindow(String link, GridPane gridPane) throws IOException {
+        gridPane.getChildren().clear();
+        int i = 0, j = 0;
+        for (News news : getListNews(link)) {
+            // TODO
         }
     }
 
     public static void createMainWindow() throws Exception {
 
-        Scene scene = new Scene((Parent) createElementMainWindow(), 800, 500);
+        Scene scene = new Scene((Parent) createElementMainWindow(), 1200, 700);
         scene.getStylesheets().add(PATH_TO_STYLE);
 
         pPrimaryStage.setScene(scene);
